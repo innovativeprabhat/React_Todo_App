ff7e85eefbcc9500ecf33228bbc42f5e7a52bd11
innovativeprabhat <abc.nmnvjj@gmail.com>
2021-05-02 19:25:22 +0530

    add form and todo components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Components/form'
+import to from './Components/todolist'
+import todolist from './Components/todolist';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Todo List</h1>
+      <Form></Form>
+      <todolist></todolist>
     </div>
   );
 }
---
+++ src/Components/todolist.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const todolist=()=>
+{
+
+    return (
+
+     <div class="todo-container">
+        <ul class="todo-list"></ul>
+      </div>
+
+    )
+    ;
+
+}
+
+export default todolist
